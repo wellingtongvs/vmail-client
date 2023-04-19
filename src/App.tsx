063ad3823d319ca0b8bdde9647d6d3bfe3d1a54e
Wellington Gabriel Vicente de Souza <wellingtongabrielvs@gmail.com>
2023-04-19 00:25:52 -0300

    [feature/auth] basic router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import { Login } from './pages/Login';
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AppContainer>
+            <Routes>
+                <Route path="/login" element={<Login />}></Route>
+            </Routes>
+        </AppContainer>
+    );
 }
 
 export default App;
